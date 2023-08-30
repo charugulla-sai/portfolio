840cf4ff5559e736614140b417a112dff1833bd7
mani-redy-nevoori <satyasaicharugull.3@gmail.com>
2023-08-30 12:28:23 +0530

    add online tag status change on scroll code

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const header = document.querySelector('.primary-header');
+const ownerName = document.querySelector('.owner-name');
+const onlineTagContent = document.querySelector('.online-tag p');
+const onlineTagStatus = document.querySelector('.dot');
 
-window.addEventListener('scroll', ()=>{
-  if(window.scrollY > 0){
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
     header.classList.add('header-active');
-  }else{
+    ownerName.classList.add('owner-name-active');
+    onlineTagContent.textContent = "Available";
+    onlineTagStatus.style.backgroundColor= '#78e009'
+    onlineTagStatus.style.boxShadow= '0 0 10px 3px #85e80cda'
+
+  } else {
     header.classList.remove('header-active');
+    ownerName.classList.remove('owner-name-active');
+    onlineTagContent.textContent = "Busy";
+    onlineTagStatus.style.backgroundColor= '#e80c0c'
+    onlineTagStatus.style.boxShadow= '0 0 10px 3px #e80c0c'
+
   }
-})
+});
