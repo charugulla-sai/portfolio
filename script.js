c7ceabda94b8290e9f86e51c4b7a5584cebf058d
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2023-09-19 18:55:15 +0530

    Header updated

--- script.js
+++ script.js
@@ -2,23 +2,21 @@
 
 const header = document.querySelector('.primary-header');
 const ownerName = document.querySelector('.owner-name');
-const onlineTagContent = document.querySelector('.online-tag p');
+// const onlineTagContent = document.querySelector('.online-tag p');
 const onlineTagStatus = document.querySelector('.dot');
 
-const educationSection = document.querySelector('.education-section');
-const truckIcon = document.querySelector('.truck-icon > svg');
+// const educationSection = document.querySelector('.education-section');
+// const truckIcon = document.querySelector('.truck-icon > svg');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     header.classList.add('header-active');
     ownerName.classList.add('owner-name-active');
-    onlineTagContent.textContent = 'Available';
     onlineTagStatus.style.backgroundColor = '#78e009';
     onlineTagStatus.style.boxShadow = '0 0 10px 3px #85e80cda';
   } else {
     header.classList.remove('header-active');
     ownerName.classList.remove('owner-name-active');
-    onlineTagContent.textContent = 'Busy';
     onlineTagStatus.style.backgroundColor = '#e80c0c';
     onlineTagStatus.style.boxShadow = '0 0 10px 3px #e80c0c';
   }
