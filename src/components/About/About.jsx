1eb0287e28eb4e88cade3a053e4692bf802deda4
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-05-16 17:45:14 +0530

    add about component.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,37 @@
+import aiImage from '../../img/aiimage.png';
+import './About.css';
+export default function About() {
+  return (
+    <section className="bg-blue-700 w-full p-5">
+      <div className=" w-[1100px] mx-auto">
+        <h3 className="about_me_heading font-bold text-6xl max-w-fit mb-6">
+          About Me
+        </h3>
+        <div className="flex gap-8 items-center">
+          <div className="about_image_box min-w-[320px]">
+            <img className="w-full" src={aiImage} />
+          </div>
+          <div className="about_text_box text-[#ececec] leading-7 text-[17px]">
+            <p className='mb-2'>
+              Welcome to my portfolio! With 2.5 years of experience as a React
+              JS developer, I specialize in crafting dynamic and responsive web
+              applications. From concept to execution, I'm dedicated to
+              delivering clean, efficient code that enhances user experiences.
+              Passionate about staying at the forefront of technology, I'm
+              committed to continuous learning and growth to ensure that my work
+              remains innovative and impactful.
+            </p>
+            <p>
+              Driven by a love for problem-solving and collaboration, I thrive
+              in team environments where I can contribute my expertise and
+              creativity. Whether it's building intuitive UI components or
+              optimizing performance, I approach each project with enthusiasm
+              and a focus on exceeding expectations. Let's work together to
+              create something amazing!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,61 @@
+import { useState } from 'react';
+import './Hero.css';
+import myimage from '../../img/download.png';
+import aiImage from '../../img/aiimage.png';
 export default function Hero() {
+  const [profileImg, setProfileImg] = useState(aiImage);
   return (
     <>
-      <main className="w-full">
-        <div className="max-w-[900px] mx-auto  p-5 flex gap-4 justify-between items-center">
-          <div className="text_box max-w-md flex flex-col gap-5">
-            <h1 className="font-bold text-5xl">Front-end React Developer 👋🏻</h1>
-            <p>
-              Hi, I'm Charugulla Sai. A passionate Front-end React developer
-              based in Hyderabad, Telangana. 📍
+      <main className="w-full  ">
+        <div className="max-w-[1100px] mx-auto  p-5 flex gap-6 justify-between items-center">
+          <div className="text_box max-w-xl flex flex-col">
+            <p className="text-blue-500 font-semibold">Hi, my name is</p>
+            <h1 className="font-bold text-5xl mb-1 ">Charugulla Sai.</h1>
+            <h1 className="font-bold text-[38px] text-gray-600 mb-2">
+              I build things for the web.
+            </h1>
+            <p className="mb-3">
+              I'm a passionate Fullstack developer mainly focussing on Front-end
+              <span className="text-blue-600 font-bold text-lg"> React </span>
+              Development.
             </p>
+            <a
+              href="https://maps.app.goo.gl/tex8kWQbtp3Rj3hTA"
+              className="flex gap-2 hover:cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 text-blue-700"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                />
+              </svg>
+              <p>Hyderabad, India</p>
+            </a>
           </div>
-          <div className="image_box max-w-80 rounded-[50%] overflow-hidden">
-            <img className="w-full" src="src\img\ai image.png" />
+          <div className="hero_image_box max-w-80 rounded-[50%] overflow-hidden border-solid border-black border-4 ">
+            <img
+              className="w-full "
+              onMouseEnter={() => {
+                setProfileImg(myimage);
+              }}
+              onMouseLeave={() => {
+                setProfileImg(aiImage);
+              }}
+              src={profileImg}
+            />
           </div>
         </div>
         <div className="max-w-[900px] mx-auto flex justify-start p-5 items-center">
