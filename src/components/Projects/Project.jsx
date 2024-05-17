c4cbeb4183821be64214d38181f5b32eb858c92a
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-05-17 21:47:31 +0530

    add projects section.

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,32 +1,82 @@
-import omniImg from '../../img/omnifood.png';
-export default function Project() {
+import './Project.css';
+export default function Project({
+  projectImg,
+  projectTitle,
+  projectDesc,
+  techUsed,
+  githubLink,
+  liveDemoLink,
+}) {
   return (
-    <div className="flex w-full gap-9 bg-yellow-300 items-center justify-between">
-      <div className="project_img_box min-w-[48%] max-h-[320px] overflow-hidden  hover:transform-[100%] transition transition-all">
-        <img className="w-full" src={omniImg} alt="project image" />
+    <div className="project_card flex w-full gap-9 p-5 rounded-tl-[16px] items-center justify-between hover:scale-[1.01] ">
+      <div className="project_img_box min-w-[48%] max-h-[320px] overflow-hidden">
+        <img
+          className="project_img w-full "
+          src={projectImg}
+          alt={projectImg}
+        />
       </div>
       <div className="project_text_box">
-        <h4 className="project_title">Omnifood</h4>
-        <p className="project_desc">
-          Welcome to my portfolio! With 2.5 years of experience as a React JS
-          developer, I specialize in crafting dynamic and responsive web
-          applications. From concept to execution, I'm dedicated to delivering
-          clean, efficient code that enhances user experiences. Passionate about
-          staying at the forefront of technology, I'm committed to continuous
-          learning and growth to ensure that my work remains innovative and
-          impactful. Driven by a love for problem-solving and collaboration, I
-          thrive in team
-        </p>
-        <div className="tech_used">
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Vanilla JS</li>
+        <h4 className="project_title font-semibold text-2xl uppercase mb-2">
+          {projectTitle}
+        </h4>
+        <div className="project_desc mb-4">
+          <p>{projectDesc}</p>
+        </div>
+        <div className="tech_used mb-4">
+          <ul className="flex gap-4">
+            {techUsed.map((technology) => (
+              <li className="w-8 hover:scale-105">
+                <img className="w-full" src={technology} />
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="project_links">
-          <p>Github</p>
-          <p>Live Demo</p>
+        <div className="project_links flex  gap-8 text-blue-700  text-lg ">
+          <a
+            className="flex gap-1 items-center "
+            href={githubLink}
+            target="_blank"
+          >
+            Github
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-github w-5"
+            >
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </svg>
+          </a>
+          <a
+            className="flex gap-1 items-center"
+            href={liveDemoLink}
+            target="_blank"
+          >
+            Live Demo
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-external-link w-5"
+            >
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+              <polyline points="15 3 21 3 21 9"></polyline>
+              <line x1="10" y1="14" x2="21" y2="3"></line>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
--- src/components/Projects/ProjectContainer.jsx
+++ src/components/Projects/ProjectContainer.jsx
@@ -1,14 +1,62 @@
 import Project from './Project';
+import omniImg from '../../img/omnifood.png';
+import daruwalaImg from '../../img/daruwala.png';
+import html from '../../img/html-5.png';
+import css from '../../img/css-3.png';
+import js from '../../img/js.png';
+import reactjs from '../../img/reactjs.png';
+import nodejs from '../../img/nodejs.png';
+import expressjs from '../../img/expressjs.png';
+import mongodb from '../../img/mongodb.png';
+import tailwindcss from '../../img/tailwindcss.png';
+
+const projects = [
+  {
+    projectImg: daruwalaImg,
+    projectTitle: 'Project 2',
+    projectDesc: `Introducing Daruwala, a sophisticated alcohol-centric website meticulously crafted with the MERN stack. Featuring seamless user experiences like sign-in, sign-up, cart management, orders, and secure payments via Razorpay, Daruwala redefines the way enthusiasts explore and purchase their favorite spirits. Leveraging MongoDB Atlas for robust data management, this platform seamlessly integrates modern design with cutting-edge functionality, offering a one-stop destination for aficionados to indulge in the world of fine libations.`,
+    techUsed: [reactjs, nodejs, expressjs, mongodb, css],
+    githubLink: 'https://github.com/charugulla-sai/daruwala.git',
+    liveDemoLink: 'https://daruwala.vercel.app',
+  },
+  {
+    projectImg: omniImg,
+    projectTitle: 'Omnifood',
+    projectDesc: `Welcome to Omnifood – your gateway to culinary excellence. Our
+    homepage, built with HTML, CSS, and JavaScript, offers a visually
+    stunning and user-friendly experience. Explore a diverse collection
+    of mouthwatering recipes and engage with a vibrant community of food
+    enthusiasts. Let Omnifood inspire your next culinary adventure. Bon
+    appétit!`,
+    techUsed: [html, css, js],
+    githubLink: 'https://github.com/charugulla-sai/omnifood.git',
+    liveDemoLink: 'https://charugulla-sai.github.io/omnifood/',
+  },
+];
 
 export default function ProjectContainer() {
   return (
-    <section className="w-full">
-      <div className="max-w-[1100px] mx-auto p-5">
-        <div>
-          <h3>Projects</h3>
-          <h4>Each project is a unique peice of development.🧩</h4>
+    <section className="w-full bg-white">
+      <div className="max-w-[1100px] mx-auto p-5 ">
+        <div className="mb-9">
+          <h3 className="text-xl font-semibold uppercase text-[#4446b7] border-b-2 border-b-red-200 mx-auto w-fit mb-7">
+            Projects
+          </h3>
+          <h4 className='text-2xl font-bold'>Each project is a unique peice of development.🧩</h4>
         </div>
-      <Project />
+        <div className="flex flex-col gap-9">
+          {projects.map((project) => (
+            <Project
+              key={project.projectImg}
+              projectImg={project.projectImg}
+              projectTitle={project.projectTitle}
+              projectDesc={project.projectDesc}
+              techUsed={project.techUsed}
+              githubLink={project.githubLink}
+              liveDemoLink={project.liveDemoLink}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
